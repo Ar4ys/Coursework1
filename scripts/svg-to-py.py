dbe01de3bebf77e03546f777a9db7cbc2bd0f013
Ar4ys <ar4ys@pm.me>
2022-06-07 18:37:43 +0200

    Added printing logic to scripts/svg-to-py

--- scripts/svg-to-py.py
+++ scripts/svg-to-py.py
@@ -1,17 +1,277 @@
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, Any
 from svgelements import SVG, Path, PathSegment, Line, Close, Point, Rect, Circle, Ellipse, SVGElement, Group
 from tkinter import Tk, Canvas
 
+SimplePoint = Tuple[float | int, float | int]
+
+
+class Printer:
+    indent_symbol = ' '
+    indent_size = 4
+    indent = 0
+    buffer = ''
+
+    def __init__(self) -> None:
+        self.print_helpers()
+
+    def print_group(self, group: Group):
+        self.print_def(group.id)
+
+        elem: SVGElement
+        for elem in group:
+            match elem:
+                case Path():
+                    self.print_path(elem)
+                case Rect():
+                    self.print_rect(elem)
+                case Circle() | Ellipse():
+                    self.print_oval(elem)
+                case Group():
+                    self.print_group(elem)
+
+        self.indent_down()
+        self.print(f'{group.id}()')
+
+    def print_rect(self, rect: Rect):
+        coords = rect_coords(rect)
+
+        if not coords:
+            return
+
+        self.print_simple_2d(
+            'draw_rect',
+            coords=coords,
+            fill=rect.fill.hex,
+            outline=rect.stroke.hex,
+            width=rect.stroke_width
+        )
+
+    def print_oval(self, oval: Ellipse | Circle):
+        coords = oval_coords(oval)
+
+        if not coords:
+            return
+
+        self.print_simple_2d(
+            'draw_oval',
+            coords=coords,
+            fill=oval.fill.hex,
+            outline=oval.stroke.hex,
+            width=oval.stroke_width
+        )
+
+    def print_simple_2d(
+        self,
+        name: str,
+        coords: Tuple[SimplePoint, ...],
+        fill: str | None = None,
+        outline: str | None = None,
+        width: float | None = None
+    ):
+        to_print = f'{name}('
+        coords = int_coords(coords) if parse_coords_int else coords
+
+        if not fill and not outline and not width:
+            to_print += f'{coords})'
+            self.print(to_print)
+            return
+
+        if print_fun_in_line:
+            to_print += f'{coords}'
+            to_print += f", fill='{fill}'" if fill else ''
+            if outline:
+                to_print += f", outline='{outline}'" if outline not in ('#000', '#000000') else ''
+                to_print += f', width={width}' if width else ''
+            to_print += f')'
+            self.print(to_print)
+        else:
+            self.print(to_print)
+            self.indent_up()
+            self.print(
+                f'{coords},',
+                f"fill='{fill}'," if fill else None,
+            )
+            if outline:
+                self.print(
+                    f"outline='{outline}'," if outline not in ('#000', '#000000') else None,
+                    f'width={width},' if width else None
+                )
+            self.indent_down()
+            self.print(')')
+
+    def print_path(self, path: Path):
+        if is_path_closed(path):
+            self.print_polygon(path)
+        else:
+            self.print_simple_2d(
+                'draw_path',
+                path_coords(path),
+                outline=path.stroke.hex,
+                width=path.stroke_width,
+            )
+
+    def print_helpers(self):
+        self.print_split_lines("""
+def draw_oval(
+    coords: Tuple[Tuple[float, float], ...],
+    fill: str | None = None,
+    outline: str | None = None,
+    width: float | None = None
+):
+    canvas.create_oval(
+        coords,
+        fill=fill or '',
+        outline=outline or '#000',
+        width=width or 0
+    )
+
+
+def draw_rect(
+    coords: Tuple[Tuple[float, float], ...],
+    fill: str | None = None,
+    outline: str | None = None,
+    width: float | None = None
+):
+    canvas.create_rectangle(
+        coords,
+        fill=fill or '',
+        outline=outline or '#000',
+        width=width or 0
+    )
+
+
+def draw_polygon(
+    coords: Tuple[Tuple[float, float], ...],
+    fill: str | None = None,
+    outline: str | None = None,
+    width: float | None = None
+):
+    outline = outline or ('#000' if width else '')
+    canvas.create_polygon(
+        coords,
+        fill=fill or '',
+        outline=outline,
+        width=width or 0
+    )
+
+
+def draw_path(
+    coords: Tuple[Tuple[float, float], ...],
+    outline: str | None = None,
+    width: float | None = None
+):
+    for (i, point) in enumerate(coords):
+        if i == 0:
+            continue
+
+        canvas.create_line(
+            (coords[i - 1], point),
+            fill=outline or '#000',
+            width=width or ''
+        )
+""")
+
+    def print_line(
+        self,
+        coords: Tuple[SimplePoint, SimplePoint],
+        fill: str | None = None,
+        width: float | None = None
+    ):
+        self.print_simple_2d(
+            'canvas.create_line',
+            coords,
+            fill=fill,
+            width=width,
+        )
+
+    def print_polygon(self, path: Path):
+        coords = path_coords(path)
+
+        if not coords:
+            return
+
+        self.print_simple_2d(
+            'draw_polygon',
+            coords=coords,
+            fill=path.fill.hex,
+            outline=path.stroke.hex,
+            width=path.stroke_width
+        )
+
+    def print_def(self, name, *params: str):
+        fun_def = f'def {name}('
+
+        if len(params) < 3:
+            for param in params:
+                fun_def += f'{param}'
+
+            fun_def += f'):'
+            self.print(fun_def)
+            self.indent_up()
+            return
+
+        self.print(fun_def)
+        self.indent_up()
+
+        for (i, param) in enumerate(params):
+            comma = ',' if i != len(params) - 1 else ''
+            self.print(param + comma)
+
+        self.indent_down()
+        self.print('):')
+        self.indent_up()
+
+    def indent_set(self, value: int):
+        self.indent = value
+
+    def indent_up(self):
+        self.indent += 1
+
+    def indent_down(self):
+        self.indent -= 1
+
+    def print(self, *strings: str | None):
+        indent = self.indent_symbol * self.indent_size * self.indent
+        for string in strings:
+            if string:
+                self.buffer += indent + string + '\n'
+
+    def print_split_lines(self, string: str):
+        for line in string.splitlines():
+            self.print(line)
 
 def coords(*args: Point | Tuple[float, float]):
     return tuple(map(lambda x: (x[0], x[1]), args))
 
 
+def int_coords(coords: SimplePoint | Tuple[SimplePoint | Tuple[SimplePoint, ...], ...]) -> Any:
+    return tuple(map(lambda x: int_coords(x) if isinstance(x, Tuple) else int(x), coords))
+
+
+def path_coords(path: Path, closed=False):
+    path_points: List[Tuple[float, float]] = []
+    segments: List[PathSegment] = path.segments()
+    for (i, segment) in enumerate(segments):
+        match segment:
+            case Line() | Close():
+                if len(path_points) == 0:
+                    path_points.append(coords(segment.start or Point(0, 0))[0])
+
+                if closed and i == len(segments) - 1:
+                    continue
+
+                path_points.append(coords(segment.end or Point(0, 0))[0])
+
+    return tuple(path_points)
+
 def rect_coords(rect: Rect):
-    if rect.x and rect.y:
-        return rect.x, rect.y, rect.x + rect.width, rect.y + rect.height
-    return None
+    if not rect.width or not rect.height:
+        return None
+
+    x = rect.x or 0
+    y = rect.y or 0
+    return (x, y), (x + rect.width, y + rect.height)
 
 
 def oval_coords(oval: Circle | Ellipse):
@@ -119,10 +379,16 @@ svg: SVG = SVG.parse(path_to_svg)
 
 window: Tk
 canvas: Canvas
+printer: Printer
+
+parse_coords_int = True
+print_fun_in_line = True
 
 match command:
     case 'compile':
-        print('No')
+        printer = Printer()
+        printer.print_group(svg)
+        print(printer.buffer)
     case 'view':
         window = init_window()
         canvas = init_canvas(window)
